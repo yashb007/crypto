b4d22f283f1b8030f91e1dc36aa61d1a0a380957
yashb007 <bansaly37@gmail.com>
2020-08-19 17:33:53 +0530

    approve function done

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,7 +13,15 @@ contract Token{
         uint256 _value
     );
 
+    
+    event  Approval(
+        address indexed _owner ,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
     
 
     constructor(uint256 _initialSupply) public{
@@ -29,4 +37,12 @@ contract Token{
       emit Transfer(msg.sender, _to , _value);
       return true;
     }
+
+    function approve (address _spender, uint256 _value) public returns ( bool success){
+          allowance[msg.sender][_spender] = _value;
+            emit Approval(msg.sender, _spender, _value);
+               return true; 
+    }
+
+
 }
--- test/Token.js
+++ test/Token.js
@@ -63,5 +63,26 @@ contract('Token',function(accounts){
         console.log(balance)
         assert.equal(balance.toNumber(),'750000','deduct the amount from the sender');
        })
+});
+
+it('approves token for delegated transfer',function(){
+    return Token.deployed().then(function(instance){
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1],100);
+    }).then(function(success){
+        assert.equal(success,true,"it returns true")
+        return tokenInstance.approve(accounts[1],100, {from : accounts[0]})
+}).then(function(receipt){
+             assert.equal(receipt.logs.length,1,'triger event')    
+             assert.equal(receipt.logs[0].event,'Approval','should be "Approval" event' )    
+             assert.equal(receipt.logs[0].args._owner,accounts[0],"token are authorized by")    
+             assert.equal(receipt.logs[0].args._spender,accounts[1],"tokens are authorized to")    
+             assert.equal(receipt.logs[0].args._value,100,"transfer amount")    
+             return tokenInstance.allowance(accounts[0],accounts[1]);
+}).then(function(allowance){
+    assert.equal(allowance,100,"stores allowance for transfer")
 })
+})
+
+
 })
