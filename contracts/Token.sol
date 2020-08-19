bb8065ac3d2b039923d30e48fa1de829bf819aaa
yashb007 <bansaly37@gmail.com>
2020-08-19 18:36:37 +0530

    transfer from function done

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -43,6 +43,17 @@ contract Token{
             emit Approval(msg.sender, _spender, _value);
                return true; 
     }
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
 
+        allowance[_from][msg.sender] -= _value;
+
+         emit  Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
